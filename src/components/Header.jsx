6c7262733d9f7b8d30a51c77fd13ca20fe55075f
Abel Mesele <meseleabel99@gmail.com>
2025-03-06 12:35:47 +0300

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import RestaurantLogo from "../assets/logo.jpg";
+
+export const Header = () => {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={RestaurantLogo} alt="" />
+        <h1>Something Cafe</h1>
+      </div>
+      <nav>
+        <button>Cart(0)</button>
+      </nav>
+    </header>
+  );
+};
